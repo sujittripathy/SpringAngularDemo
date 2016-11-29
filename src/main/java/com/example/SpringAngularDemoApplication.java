75aaa40b5323736dfd0072aa842a575009016e57
stripathy <sujit.tripathy@icloud.com>
2016-11-29 14:09:48 -0800

    Initial commit

---
+++ src/main/java/com/example/SpringAngularDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularDemoApplication.class, args);
+	}
+}
